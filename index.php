93c9eedb4959faec45c5000e21b148cb3f2dc073
Edwin Gonzalez <edgoca1962@gmail.com>
2023-03-14 10:21:42 -0600

    Index

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+get_header();
+if (is_user_logged_in()) {
+   if (have_posts()) {
+      while (have_posts()) {
+         echo '<h3>Theme Framework Index.php</h3>';
+         the_post();
+         get_template_part('');
+      }
+   } else {
+      get_template_part('');
+   }
+} else {
+   if (is_page('login')) {
+      get_template_part('');
+   } elseif (is_front_page()) {
+      get_template_part('');
+   } else {
+      get_template_part('');
+   }
+}
