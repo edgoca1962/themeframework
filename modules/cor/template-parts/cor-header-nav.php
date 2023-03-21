d7efc866fed87c5339987422f45db7d0172f749a
Edwin Gonzalez <edgoca1962@gmail.com>
2023-03-21 08:34:53 -0600

    Posts

--- modules/cor/template-parts/cor-header-nav.php
+++ modules/cor/template-parts/cor-header-nav.php
@@ -1,7 +1,7 @@
 <header>
-   <nav id="main_navbar" class="navbar navbar-expand-lg navbar-dark fixed-top shadow text-white" data-menubg="no">
+   <nav id="main_navbar" class="navbar navbar-expand-lg navbar-dark fixed-top shadow">
       <div class="container">
-         <div id="logo" class="logo">
+         <div id="logo" class="navbar-brand logo">
             <?php
             if (has_custom_logo()) { ?>
                <div class="d-flex justify-content-center">
@@ -17,90 +17,18 @@
             <span class="navbar-toggler-icon"></span>
          </button>
          <div class="justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0">
-               <li class="nav-item">
-                  <a class="nav-link <?php if (is_page('front-page')) echo 'active' ?>" aria-current="page" href="<?php echo esc_url(site_url('/')) ?>">Home</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-               </li>
-               <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle 
-                  <?php if (isset($_GET['menu'])) {
-                     $menu_id = intval(sanitize_text_field($_GET['menu']));
-                     if ($menu_id > 30000 & $menu_id < 40000) {
-                        echo 'active';
-                     }
-                  } ?>" href="#" role="button" data-bs-auto-close="outside" data-bs-toggle="dropdown" aria-expanded="false">
-                     Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                     <li class="dropstart">
-                        <a href="#" class="dropdown-item dropdown-toggle
-                        <?php if (isset($_GET['menu'])) {
-                           $menu_id = intval(sanitize_text_field($_GET['menu']));
-                           if ($menu_id > 31000 & $menu_id < 32000) {
-                              echo 'active';
-                           }
-                        } ?>" data-bs-auto-close="outside" data-bs-toggle="dropdown">Submenu</a>
-                        <ul class="dropdown-menu">
-                           <li><a href="<?php echo esc_url(site_url('/sample-page')) . '?menu=31100' ?>" class="dropdown-item 
-                           <?php if (isset($_GET['menu'])) {
-                              $menu_id = intval(sanitize_text_field($_GET['menu']));
-                              if ($menu_id == 31100) {
-                                 echo 'active';
-                              }
-                           } ?>">Item 1</a></li>
-                           <li><a href="<?php echo esc_url(site_url('/about')) . '?menu=31200' ?>" class="dropdown-item 
-                           <?php if (isset($_GET['menu'])) {
-                              $menu_id = intval(sanitize_text_field($_GET['menu']));
-                              if ($menu_id == 31200) {
-                                 echo 'active';
-                              }
-                           } ?>">Item 2</a></li>
-                           <li class="dropstart">
-                              <a href="" class="dropdown-item dropdown-toggle 
-                              <?php if (isset($_GET['menu'])) {
-                                 $menu_id = intval(sanitize_text_field($_GET['menu']));
-                                 if ($menu_id > 31300 & $menu_id < 31400) {
-                                    echo 'active';
-                                 }
-                              } ?>" role="button" data-bs-auto-close="outside" data-bs-toggle="dropdown" aria-expanded="false">Submenu 2</a>
-                              <ul class="dropdown-menu">
-                                 <li><a href="<?php echo esc_url(site_url('/sample-page')) . '?menu=31310' ?>" class="dropdown-item <?php if (is_page('sample-page')) echo 'active' ?>">Sample Page</a></li>
-                                 <li><a href="" class="dropdown-item">Item 2</a></li>
-                                 <li><a href="" class="dropdown-item">Item 3</a></li>
-                                 <li class="dropstart">
-                                    <a href="" class="dropdown-item dropdown-toggle
-                              <?php if (isset($_GET['menu'])) {
-                                 $menu_id = intval(sanitize_text_field($_GET['menu']));
-                                 if ($menu_id > 31340 & $menu_id < 31450) {
-                                    echo 'active';
-                                 }
-                              } ?>" role="button" data-bs-auto-close="outside" data-bs-toggle="dropdown" aria-expanded="false">Submenu 3</a>
-                                    <ul class="dropdown-menu">
-                                       <li><a href="<?php echo esc_url(site_url('/sca-cambio-clave')) . '?menu=31341' ?>" class="dropdown-item <?php if (is_page('sca-cambio-clave')) echo 'active' ?>">Item 1</a></li>
-                                       <li><a href="<?php echo esc_url(site_url('/level-1')) . '?menu=31342' ?>" class="dropdown-item <?php if (is_page('level-1')) echo 'active' ?>">Item 2</a></li>
-                                       <li><a href="" class="dropdown-item">Item 3</a></li>
-                                       <li><a href="" class="dropdown-item">Item 4</a></li>
-                                    </ul>
-                                 </li>
-                              </ul>
-                           </li>
-                        </ul>
-                     </li>
-                     <li><a class="dropdown-item" href="#">Another action</a></li>
-                     <li>
-                        <hr class="dropdown-divider">
-                     </li>
-                     <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link disabled">Disabled</a>
-               </li>
-            </ul>
-
+            <?php
+            if (is_user_logged_in()) {
+               wp_nav_menu(
+                  array(
+                     'theme_location'  => 'principal',
+                     'container'       => false,
+                     'menu_class'      => 'nav navbar-nav ms-auto mb-2 mb-lg-0',
+                     'walker' => new Walker_Nav_Primary()
+                  )
+               );
+            }
+            ?>
             <div id="btn_menu" class="navbar nav-item me-n3">
                <button type="button" class="btn btn-warning btn-sm">
                   <?php if (is_user_logged_in()) : ?>
--- modules/pst/template-parts/post.php
+++ modules/pst/template-parts/post.php
@@ -1,5 +1,5 @@
 <div class='col'>
-    <div class="card h-100" style="background-color: #28303d;">
+    <div class="card h-100" style="background-color: rgba(40,48,61,1);">
         <img src="<?php echo themeframework_get_page_att($post->post_type)['imagen'] ?>" class="card-img-top" alt="post image">
         <div class="card-body">
             <h5 class="card-title"><?= the_title(sprintf('<h4><a href="%s" rel="bookmark">', esc_attr(esc_url(get_permalink() . '?pag=' . themeframework_get_page_att($post->post_type)['pag']))), '</a></h4>') ?></h5>
