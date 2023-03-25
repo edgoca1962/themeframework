0d23f6cb854ba8a5ddd66683c8bb794417afa3dd
Edwin Gonzalez <edgoca1962@gmail.com>
2023-03-25 08:06:23 -0600

    Acuerdo Mant

--- modules/sca/template-parts/acuerdo.php
+++ modules/sca/template-parts/acuerdo.php
@@ -17,37 +17,33 @@
             </small>
          </p>
       </div>
-      <?php if (themeframework_get_page_att($post->post_type)['userAdmin']) { ?>
+      <?php if (themeframework_get_page_att($post->post_type)['userAdmin']) : ?>
+         <div class="card-footer">
+            <div class="row">
+               <div class="col-sm-12 col-md-8">
+                  <?php echo 'Comité: ' . get_post(get_post_meta(get_the_ID(), '_comite_id', true))->post_title ?>
+               </div>
+               <!-- <div class="w-100"></div> -->
+               <div class="col-sm-12 col-md-4">
+                  <!-- Button trigger modal editar -->
+                  <button type="button" class="btn btn-outline-warning btn-sm me-3" data-bs-toggle="modal" data-bs-target="#editar" data-editar="<?php echo get_the_ID() ?>" data-url="<?php echo get_site_url() . '/wp-json/wp/v2/acuerdos/' . get_the_ID() ?>" data-usr_id=<?php echo get_post_meta(get_the_ID(), '_asignar_id', true) ?>><i class="fa-solid fa-pencil" style="font-size: 12px;"></i> Editar</button>
+                  <button type="button" class="btn btn-outline-danger btn-sm" data-post_id="<?php echo get_the_ID() ?>" data-eliminar="elemento_<?php echo get_the_ID() ?>"><i class="fa-solid fa-trash-can" style="font-size: 12px;"></i> Eliminar</button>
 
-         <div class="d-flex card-footer">
-            <div class="col-6">
-               <?php echo 'Comité: ' . get_post(get_post_meta(get_the_ID(), '_comite_id', true))->post_title ?>
-            </div>
-            <div class="col-6">
-               <div class="row">
-                  <div class="col-6">
-                     <!-- Button trigger modal editar -->
-                     <button type="button" class="btn btn-outline-warning btn-sm me-3" data-bs-toggle="modal" data-bs-target="#editar" data-editar="<?php echo get_the_ID() ?>" data-url="<?php echo get_site_url() . '/wp-json/wp/v2/acuerdos/' . get_the_ID() ?>" data-usr_id=<?php echo get_post_meta(get_the_ID(), '_asignar_id', true) ?>><i class="fa-solid fa-pencil" style="font-size: 12px;"></i> Editar</button>
-                  </div>
-                  <div class="col-6">
-                     <button type="button" class="btn btn-outline-danger btn-sm" data-post_id="<?php echo get_the_ID() ?>" data-eliminar="elemento_<?php echo get_the_ID() ?>"><i class="fa-solid fa-trash-can" style="font-size: 12px;"></i> Eliminar</button>
-
-                     <form id="<?php echo get_the_ID() ?>">
-                        <input type="hidden" name="action" value="eliminar_acuerdo">
-                        <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('eliminar_acuerdo') ?>">
-                        <input type="hidden" name="endpoint" value="<?php echo admin_url('admin-ajax.php') ?>">
-                        <input type="hidden" name="post_id" value="<?php echo get_the_ID() ?>">
-                     </form>
+                  <form id="<?php echo get_the_ID() ?>">
+                     <input type="hidden" name="action" value="eliminar_acuerdo">
+                     <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('eliminar_acuerdo') ?>">
+                     <input type="hidden" name="endpoint" value="<?php echo admin_url('admin-ajax.php') ?>">
+                     <input type="hidden" name="post_id" value="<?php echo get_the_ID() ?>">
+                  </form>
 
 
-                     <input id="titulo_elemento_<?php echo get_the_ID() ?>" class="invisible" type="hidden" value="<?php echo 'Acuerdo ' . get_post_meta(get_the_ID(), '_n_acuerdo', true) ?>">
-                     <input id="msg_elemento_<?php echo get_the_ID() ?>" class="invisible" type="hidden" value="Se eliminará este acuerdo del Acta.">
-                     <input id="msg2_elemento_<?php echo get_the_ID() ?>" class="invisible" type="hidden" value="El acuerdo ha sido eliminado.">
-                  </div>
+                  <input id="titulo_elemento_<?php echo get_the_ID() ?>" class="invisible" type="hidden" value="<?php echo 'Acuerdo ' . get_post_meta(get_the_ID(), '_n_acuerdo', true) ?>">
+                  <input id="msg_elemento_<?php echo get_the_ID() ?>" class="invisible" type="hidden" value="Se eliminará este acuerdo del Acta.">
+                  <input id="msg2_elemento_<?php echo get_the_ID() ?>" class="invisible" type="hidden" value="El acuerdo ha sido eliminado.">
                </div>
             </div>
          </div>
-      <?php } ?>
+      <?php endif; ?>
    </div>
 </div>
 <input id="msg_<?php echo get_the_ID() ?>" type="hidden" value="<?php echo get_post_meta(get_the_ID(), '_n_acuerdo', true) ?>">
